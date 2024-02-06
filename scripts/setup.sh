6c99f5d4a02cf9fbf9e60108f5dfbdab2ae28a3a
Ubuntu <ubuntu@ip-172-31-29-82.us-west-2.compute.internal>
2024-02-06 07:40:24 +0000

    Add install criu script

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python3-pip libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf libcap-dev libnl-3-dev xmlto asciidoc protobuf-c-compiler  asciidoc libbsd-dev libdrm-dev libdrm-amdgpu1 libgnutls28-dev libnftables-dev libnet-dev criu
+git clone https://github.com/checkpoint-restore/criu.git
+cd criu
+sudo make install
+criu --version
